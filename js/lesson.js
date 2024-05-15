06522d378d4ab58fba47f34b7b365141bd8fffc8
Farukh <farukh31082005@gmail.com>
2024-05-15 13:15:17 +0600

    hw3

--- js/lesson.js
+++ js/lesson.js
@@ -16,3 +16,47 @@ phoneButton.onclick = () => {
     }
 }
 
+//tab slider
+
+const tabContentBlocks = document.querySelectorAll('.tab_content_block')
+const tabContentItems = document.querySelectorAll('.tab_content_item')
+const tabsParent = document.querySelector('.tab_content_items')
+
+const hideTabContent = ()=>{
+    tabContentBlocks.forEach((item)=>{
+        item.style.display = 'none'
+    })
+    tabContentItems.forEach((item)=>{
+        item.classList.remove('tab_content_item_active')
+    })
+}
+const showTabContent = (index = 0)=>{
+    tabContentBlocks[index].style.display = 'block'
+    tabContentItems[index].classList.add('tab_content_item_active')
+}
+
+hideTabContent()
+showTabContent()
+
+
+let tabIndex = 0
+const tabCount = tabContentItems.length
+
+const scrollTabContent = ()=>{
+    hideTabContent()
+    tabIndex = (tabIndex + 1) % tabCount
+    showTabContent(tabIndex)
+}
+
+const interval = setInterval(scrollTabContent,3000)
+tabsParent.onclick = (event)=> {
+    if(event.target.classList.contains('tab_content_item')){
+        tabContentItems.forEach((item,index)=>{
+            if(event.target === item){
+                hideTabContent()
+                showTabContent(index)
+            }
+        })
+    }
+    clearInterval(interval)
+}
